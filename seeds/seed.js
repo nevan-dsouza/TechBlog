78d89567c759174e577df9d7c5696db6e678d6d0
Nevan <nev.kd2832@gmail.com>
2022-03-27 17:06:40 -0400

    added code to seed database

---
+++ seeds/seed.js
@@ -0,0 +1,20 @@
+const sequelize = require('../config/connection');
+const {User, Post, Comment}  = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force:true });
+    
+    const user = await User.bulkCreate(userData);
+
+    const post = await Post.bulkCreate(postData);
+
+    const comment = await Comment.bulkCreate(commentData);
+
+    process.exit(0);
+}
+
+seedDatabase();
